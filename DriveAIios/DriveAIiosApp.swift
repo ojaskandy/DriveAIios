90056af5841730867ebdd6fd038cada18e5d8c1f
Ojas Kandhare <ojaskandhare@gmail.com>
2025-03-28 08:35:11 -0700

    Initial Commit

---
+++ DriveAIios/DriveAIiosApp.swift
@@ -0,0 +1,17 @@
+//
+//  DriveAIiosApp.swift
+//  DriveAIios
+//
+//  Created by Ojas Kandhare on 3/28/25.
+//
+
+import SwiftUI
+
+@main
+struct DriveAIiosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DriveAIiosTests/DriveAIiosTests.swift
@@ -0,0 +1,17 @@
+//
+//  DriveAIiosTests.swift
+//  DriveAIiosTests
+//
+//  Created by Ojas Kandhare on 3/28/25.
+//
+
+import Testing
+@testable import DriveAIios
+
+struct DriveAIiosTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
